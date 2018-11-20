75fae07395420afe6ad4ee691ff9beedc9a7f9af
Gabriel Rodrigues Segalla <gabriel.segalla@agrosatelite.com.br>
2018-11-20 10:33:54 -0200

    arquivos iniciais

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Webservice Financiamento',
+      version='1.0',
+      description='plicação WebService REST para calculos financeiros',
+      author='Gabriel Rodrigues Segalla',
+      author_email='grsegalla@hotmail.com',
+      url='https://github.com/gabrielsegalla/Webservice-Financiamento',
+      packages=['distutils', 'distutils.command'],
+     )
