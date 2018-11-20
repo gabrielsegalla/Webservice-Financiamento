4c9cc4ee2bbb5f5e88110bc4b6113c10eed21921
Gabriel Rodrigues Segalla <gabriel.segalla@agrosatelite.com.br>
2018-11-20 12:04:29 -0200

    ajustes docuemntação

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from flasgger import Swagger
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask.ext.cache import flask_cache
 from flask.ext.heroku import Heroku
 import os
 
